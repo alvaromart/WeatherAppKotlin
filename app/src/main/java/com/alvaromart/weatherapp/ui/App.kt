4ad2e1525023e6ff8bd37a9a226432eac173169c
alvaro <a.martinez@videona.com>
2019-01-15 22:54:15 +0100

    Chapter 16 Application Singleton and Delegated Properties

---
+++ app/src/main/java/com/alvaromart/weatherapp/ui/App.kt
@@ -0,0 +1,20 @@
+package com.alvaromart.weatherapp.ui
+
+import android.app.Application
+import com.alvaromart.weatherapp.ui.utils.DelegatesExt
+
+/**
+ * Created by alvaro on 15/1/19.
+ */
+
+class App : Application() {
+
+    companion object {
+        var instance: App by DelegatesExt.notNullSingleValue()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
