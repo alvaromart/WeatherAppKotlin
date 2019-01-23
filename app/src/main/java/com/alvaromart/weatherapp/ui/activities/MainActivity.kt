0767a11f0da918f269d88955fe0ff38bf1680897
alvaro <a.martinez@videona.com>
2019-01-23 23:11:27 +0100

    Chapter 24 Interfaces and Delegation

--- app/src/main/java/com/alvaromart/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/alvaromart/weatherapp/ui/activities/MainActivity.kt
@@ -3,25 +3,30 @@ package com.alvaromart.weatherapp.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
 import com.alvaromart.weatherapp.R
 import com.alvaromart.weatherapp.domain.commands.RequestForecastCommand
 import com.alvaromart.weatherapp.ui.adapters.ForecastListAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.uiThread
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), ToolbarManager {
+
+    override val toolbar by lazy { find<Toolbar>(R.id.toolbar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initToolbar()
 
         forecastList.layoutManager = LinearLayoutManager(this)
+        attachToScroll(forecastList)
 
         doAsync {
             val result = RequestForecastCommand(94043).execute()
-
             uiThread {
                 val adapter = ForecastListAdapter(result) {
                 startActivity<DetailActivity>(DetailActivity.ID to it.id,
@@ -29,6 +34,8 @@ class MainActivity : AppCompatActivity() {
                 }
                 forecastList.adapter = adapter
                 title = "${result.city} (${result.country})"
+                toolbarTitle = title.toString()
+
             }
         }
     }
